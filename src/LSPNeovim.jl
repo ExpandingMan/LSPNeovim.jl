9e1d12a131c939d58f79a2d898791308f25fd1ec
Expanding Man <savastio@protonmail.com>
2020-06-13 14:09:30 -0400

    added a little documentation

--- src/LSPNeovim.jl
+++ src/LSPNeovim.jl
@@ -15,9 +15,28 @@ depotpath() = get(ENV, "JULIA_DEPOT_PATH", Pkg.depots1())
 
 _defaultenvpath() = dirname(Base.load_path_expand("@v#.#"))
 
+"""
+    hasmanifest(dir)
+    hasmanifest()
+
+Checks whether there is a valid `Manifest.toml` in directory `dir`.  If no argument is given, it
+will check for the `Manifest.toml` in the `LSPNeovim` environment.
+"""
 hasmanifest(dir::AbstractString) = isfile(joinpath(dir,"Manifest.toml"))
 hasmanifest() = hasmanifest(PKGDIR)
 
+"""
+    envpath()
+
+Picks an appropriate environment path based on the present working directory.
+A valid environment for LanguageServer must contain an `Manifest.toml`.  Directories will be checked
+in the following order
+1. The present working directory.
+2. The immediate parent of the present working directory.
+3. The default environment directory.
+
+The first of these to contain a `Manifest.toml` will be the environment used for LanguageServer.
+"""
 function envpath(dirs=[pwd(), joinpath(pwd(),".."), _defaultenvpath()])
     dirs = filter(hasmanifest, dirs)
     if isempty(dirs)
@@ -27,6 +46,12 @@ function envpath(dirs=[pwd(), joinpath(pwd(),".."), _defaultenvpath()])
     first(dirs)
 end
 
+"""
+    run(env=envpath(), depot=depotpath(); input=stdin, output=stdout)
+
+Run the `LanguageServerInstance`.  This will also activate the `LSPNeovim` environment.
+By default, this will attempt to determine an appropriate environment, see `envpath`.
+"""
 function run(env=envpath(), depot=depotpath(); input::IO=stdin, output::IO=stdout)
     activate()
     @info("Initializing Language Server", pwd(), env, depot)
