88dab9f050a3141e75c63fcb6977dc3b581d8147
Expanding Man <savastio@protonmail.com>
2020-06-12 21:56:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# LSPNeovim.jl
+This is an attempt to smooth over the interface between [nvim-lsp](https://github.com/neovim/nvim-lsp) and [LanguageServer.jl](https://github.com/julia-vscode/LanguageServer.jl).
+
+Note that this requires at least neovim 0.5.  You can get nightly binaries as a convenient AppImage from [here](https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage).
---
+++ src/LSPNeovim.jl
@@ -0,0 +1,43 @@
+module LSPNeovim
+
+using Pkg
+using LanguageServer, SymbolServer
+
+const PKGDIR = joinpath(@__DIR__,"..")
+
+
+function activate()
+    @info("Activating LSPNeovim environment")
+    Pkg.activate(PKGDIR)
+end
+
+depotpath() = get(ENV, "JULIA_DEPOT_PATH", Pkg.depots1())
+
+_defaultenvpath() = dirname(Base.load_path_expand("@v#.#"))
+
+hasmanifest(dir::AbstractString) = isfile(joinpath(dir,"Manifest.toml"))
+hasmanifest() = hasmanifest(PKGDIR)
+
+function envpath(dirs=[pwd(), joinpath(pwd(),".."), _defaultenvpath()])
+    dirs = filter(hasmanifest, dirs)
+    if isempty(dirs)
+        @warn("Failed to find a usable environment with valid Manifest.toml.  Checked:", dirs)
+        return ""
+    end
+    first(dirs)
+end
+
+function run(env=envpath(), depot=depotpath(); input::IO=stdin, output::IO=stdout)
+    activate()
+    if !hasmanifest()
+        @info("Initializing LSPNeovim environment")
+        Pkg.update()
+    end
+    Pkg.instantiate()  # this really should never be needed, but it's here just in case
+    @info("Initializing Language Server", pwd(), env, depot)
+    s = LanguageServer.LanguageServerInstance(input, output, env, depot)
+    s.runlinter = true
+    LanguageServer.run(s)
+end
+
+end # module
