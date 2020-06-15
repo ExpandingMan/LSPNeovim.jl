9632d349e49f08dbfc8c19c5ed1333079d3e6b01
Expanding Man <savastio@protonmail.com>
2020-06-15 19:04:17 -0400

    added some more documentation

--- bin/run.jl
+++ bin/run.jl
@@ -8,5 +8,7 @@ Pkg.activate(PKGDIR)
 Pkg.instantiate()
 #==================================================================#
 
+# NOTE: SymbolServer must be used here due to a bizarre bug
+# see https://github.com/julia-vscode/LanguageServer.jl/issues/750
 using LSPNeovim, SymbolServer
 LSPNeovim.run()
