17714fc356a8792562dd56adb8425e437c3b3118
Expanding Man <savastio@protonmail.com>
2020-06-15 21:09:09 -0400

    test didn't actually work on travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,8 +8,6 @@ LSPNeovim.Pkg.activate(PKGDIR)
 LSPNeovim.Pkg.instantiate()
 
 @testset "environment" begin
-    # tests should not be able to run without the below passing
-    @test hasmanifest(LSPNeovim._defaultenvpath())
     # check that LSPNeovim manifest was generated
     @test hasmanifest()
     cd(PKGDIR)
